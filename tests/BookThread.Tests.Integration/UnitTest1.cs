cc2e34707e1bb2cf00034bf027055d03a7dd1ffa
Tumisang <tsheoletumisang934@gmail.com>
2026-01-20 11:46:07 +0000

    Added unit and integration testing projects and project references

---
+++ tests/BookThread.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace BookThread.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/BookThread.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace BookThread.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
